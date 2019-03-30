61296a852ea360335d3391bad18724435bb9aab0
hey-mikey <michaelarmes1@gmail.com>
2019-03-29 22:28:40 -0500

    WIP - Razor Pages Movie project, walking through Microsoft tutorial on Core 2.2

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        /*
+         * With this attribute:
+         * 1. The user is not required to enter time information in the date field.
+         * 2. Only the date is displayed, not time information.
+         */
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
